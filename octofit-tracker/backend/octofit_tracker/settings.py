f785efdde21dd9daebcb08a06f66cb204c3741b5
Ryan-Ceipher <ryan.j.d.membrere@accenture.com>
2026-04-16 11:06:16 +0000

    commit

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-o(c=e_sx*%e3uk$rg3_g_g%b3^i&p@jgn7s@xkej3ogsyruge_
 DEBUG = True
 
 
-# Allow all hosts for development
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace host for development
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
