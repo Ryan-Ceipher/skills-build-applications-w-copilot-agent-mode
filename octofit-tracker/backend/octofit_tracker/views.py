bd3a7eae0d5ed754de6aefd06676343867796fc0
Ryan-Ceipher <ryan.j.d.membrere@accenture.com>
2026-04-16 10:46:00 +0000

    commit

---
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -0,0 +1,5 @@
+from rest_framework import viewsets
+from .models import User, Team, Activity, Leaderboard, Workout
+from .serializers import serializers
+
+# Placeholder viewsets for users, teams, activities, leaderboard, and workouts
